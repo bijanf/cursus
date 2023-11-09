b935a6377fa5e3102a4ac75748958403dfdae5f6
Bijan Fallah <bijan.fallah@met.fu-berlin.de>
2023-11-09 13:21:04 +0100

    added the libft

---
+++ Libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bfallah- <bfallah-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/08 11:47:36 by bfallah-          #+#    #+#             */
+/*   Updated: 2023/11/08 14:53:42 by bfallah-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+
+int		ft_isalpha(int a);
+int		ft_isdigit(int a);
+int		ft_isalnum(int a);
+int		ft_isascii(int a);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
+
+#endif
