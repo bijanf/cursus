593b810ec52caf88441ac3842410dc415421b31e
bijanf <bijan.fallah@gmail.com>
2023-12-17 21:19:03 +0100

    added tests

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,7 +6,7 @@
 /*   By: bfallah- <bfallah-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 08:35:53 by bfallah-          #+#    #+#             */
-/*   Updated: 2023/12/17 21:06:33 by bfallah-         ###   ########.fr       */
+/*   Updated: 2023/12/17 21:18:26 by bfallah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@
 #include <string.h>
 #include "get_next_line.h"
 #include <stdio.h>
+
+#define COLOR_RESET "\x1b[0m"
+#define COLOR_RED "\x1b[31m"
+#define COLOR_GREEN "\x1b[32m"
+#define COLOR_YELLOW "\x1b[33m"
+#define COLOR_BLUE "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN "\x1b[36m"
 
 int main()
 {
@@ -30,21 +38,21 @@ int main()
 		write(1, "Error opening file", 18);
 		return (1);
 	}
-	while (counter <9)
+	while (counter < 14)
 	{
-		printf("--------------------\n");
+		printf(COLOR_CYAN "--------------------\n");
 
-		printf("   This is test %d    :\n", counter);
+		printf(COLOR_GREEN "   This is test %d    :\n", counter);
 		counter++;
 
 		line = get_next_line(file_descriptor);
 		if (line)
 		{
-			write(1,">>>>>  ",7);
-			write(1, line, strlen(line));
+			printf(COLOR_MAGENTA ">>>>>>>>>> ");
+			printf(COLOR_YELLOW "%s\n", line);
 			free(line);
 		}
-		printf("--------------------\n");
+		printf(COLOR_CYAN "--------------------\n");
 	}
 	close(file_descriptor);
 	return (0);
